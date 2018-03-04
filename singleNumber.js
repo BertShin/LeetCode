850efdefe6c7ad809af96d5ea4f5bf1aaccde4e8
BertShin <bertshin08@gmail.com>
2018-03-03 21:09:26 -0800

    Finish singleNumber -- JS (n log(n)) time

--- singleNumber.js
+++ singleNumber.js
@@ -6,6 +6,26 @@
 const singleNumber = (array) => {
     // CONSTANT MEMORY
     // LINEAR RUNTIME
+    // This just screams pointer to me.
+    // I could start two pointers, one at each end of the array,
+    // Compare numbers adjacent to the pointers, if the number is not equal: bingo we found our match!
+    // This would be linear time since at most we'd only iterate over the array once.
+    // AND constant space since all we're storing are pointers and no additional data structures.
+    array = array.sort();
+    let start = 0;
 
+    if (array.length === 1) {
+        return array[0];
+    }
     
+
+    while (start !== array.length) {
+        if (array[start] !== array[start + 1]) {
+            return array[start];
+        } else {
+            start += 2;
+        }
+    }
+
+    return false;
 };
